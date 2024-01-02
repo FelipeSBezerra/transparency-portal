d055e89c4a69e3a0d7907e83ac70fa29d7ee5ca3
Felipe S Bezerra <felipecrf220@gmail.com>
2024-01-02 10:37:10 -0300

    project created

---
+++ src/main/java/com/devfelipe/transparencyportal/TransparencyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.devfelipe.transparencyportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparencyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparencyPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfelipe/transparencyportal/TransparencyPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfelipe.transparencyportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransparencyPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
